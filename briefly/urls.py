55a3758e9010291af7edb2df42e579301283ccb8
大石航平 <oishikohei@ooishikouheis-MacBook-Pro.local>
2025-02-16 18:29:50 +0000

    added all the template html to show css with url path

--- briefly/urls.py
+++ briefly/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('briefly_app.urls')),
 ]
