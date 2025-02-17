a8f9300c8d3683ed07257ad9779146db62b4b218
yogesh-ch-ss <yogeshchsamant@gmail.com>
2025-02-17 16:49:19 +0000

    country field added for the brefly user.

--- briefly_app/admin.py
+++ briefly_app/admin.py
@@ -4,7 +4,17 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 from .models import BrieflyUser, Category, UserCategory, NewsArticle, ViewedNews, SavedNews
 
-admin.site.register(BrieflyUser, UserAdmin) 
+class BrieflyUserAdmin(UserAdmin):
+    model = BrieflyUser
+    fieldsets = UserAdmin.fieldsets + (  # Add the 'Country' field
+        ("Additional Info", {"fields": ("country",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (  # Include 'Country' when adding a new user
+        ("Additional Info", {"fields": ("country",)}),
+    )
+
+
+admin.site.register(BrieflyUser, BrieflyUserAdmin) 
 admin.site.register(Category)
 admin.site.register(UserCategory)
 admin.site.register(NewsArticle)
