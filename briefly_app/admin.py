429e3925b6bb4994508ad2a51db1c338b3ffd3a1
yogesh-ch-ss <yogeshchsamant@gmail.com>
2025-02-17 16:41:20 +0000

    database is setup and verified over admin

--- briefly_app/admin.py
+++ briefly_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+from .models import BrieflyUser, Category, UserCategory, NewsArticle, ViewedNews, SavedNews
+
+admin.site.register(BrieflyUser, UserAdmin) 
+admin.site.register(Category)
+admin.site.register(UserCategory)
+admin.site.register(NewsArticle)
+admin.site.register(ViewedNews)
+admin.site.register(SavedNews)
