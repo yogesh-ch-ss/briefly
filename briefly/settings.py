ed00a301091213d4f7e3a1f60e0b19385cebbada
yogesh-ch-ss <yogeshchsamant@gmail.com>
2025-03-24 18:55:32 +0000

    migrate sqlite3 to aws rds postgres

--- briefly/settings.py
+++ briefly/settings.py
@@ -127,10 +127,21 @@ WSGI_APPLICATION = 'briefly.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'briefly_psql',
+        'USER': 'postgres',
+        'PASSWORD': 'Briefly#123',
+        'HOST':'database-2.cjieqw8cauc6.eu-west-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
